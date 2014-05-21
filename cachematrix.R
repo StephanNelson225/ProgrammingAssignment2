8d03428cb7a2bfc39b7001fac46bf7e7471feb6a
Steve Nelson <nelsonsteve50@gmail.com>
2014-05-21 14:48:16 -0500

    Hope this will be the last commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,22 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The basic idea is to create functions that cache a 
+matrix so that the result doesn't have to be recalcuated every time'
 
-## Further changes.  That will show up
 
-makeCacheMatrix <- function(x = matrix()) {
-  makeVector <- function(x = numeric()) {
-    m <- NULL
-    set <- function(y) {
-      x <<- y
-      m <<- NULL
-    }
-    get <- function() x
-    setmean <- function(mean) m <<- mean
-    getmean <- function() m
-    list(set = set, get = get,
-         setmean = setmean,
-         getmean = getmean)
-  }
+
   cachemean <- function(x, ...) {
     m <- x$getmean()
     if(!is.null(m)) {
@@ -28,12 +14,27 @@ makeCacheMatrix <- function(x = matrix()) {
     x$setmean(m)
     m
   }
-  
+
+makeCacheMatrix <- function(x = matrix()) {
+     makeVector <- function(x = numeric()) {
+     m <- NULL
+    set <- function(y) {
+      x <<- y
+       m <<- NULL
+  }
+   get <- function() x
+   setmean <- function(mean) m <<- mean
+   getmean <- function() m
+  list(set = set, get = get,
+         setmean = setmean,
+         getmean = getmean)
+  }
+}
 
 }
 
 
-## Write a short comment describing this function
+## The "..." passes everything along that is elsewhere defined.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
